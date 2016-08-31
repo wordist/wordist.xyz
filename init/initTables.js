0160af5fe498c9523a1f4c923af70da984ba48f4
Venkata krishna <vkvenkat94@gmail.com>
2016-08-31 01:39:21 -0700

    Created table words.

--- init/initTables.js
+++ init/initTables.js
@@ -8,14 +8,32 @@ r.connect({ db : 'wordist'}, (err, con) => {
   if (err) {
     console.log(err);
     process.exit(1);
-  }
+  } 
+  
+  // Create users table
   r.tableCreate('users').run(con, (err, result) => {
     if (err) {
+      // Happens when table 'users' is already present. No need to exit.
+      console.log(err);
+    } else {
+      console.log('INIT users - Success.');
+      console.log(result);
+    }
+  });
+  
+  // Create words table
+  r.tableCreate('words').run(con, (err, result) => {
+    if (err) {
+      
+      // Happens when table 'words' is already present.
+      // All tables created. Exit with error code.
       console.log(err);
       process.exit(1);
+    } else {
+      
+      console.log('INIT words - Success.');
+      console.log(result);
+      process.exit(0);
     }
-    console.log('INIT users - Success.');
-    console.log(result);
-    process.exit(0);
   });
 });
