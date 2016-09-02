e4ab67d47287c9f225c1a5ebdf964971a2965606
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-09-03 01:07:14 +0530

    fixes docker build

--- dev-scripts/execute_non_pr.sh
+++ dev-scripts/execute_non_pr.sh
@@ -1,3 +1,3 @@
-docker build -t="scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER" ..
+docker build -t="scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER" .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER;
