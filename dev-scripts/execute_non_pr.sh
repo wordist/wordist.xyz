b4bc42450098f9acde9f22c1cdcbb63379f6b2be
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-09-04 11:19:36 +0530

    pushes latest docker tag

--- dev-scripts/execute_non_pr.sh
+++ dev-scripts/execute_non_pr.sh
@@ -1,3 +1,5 @@
 docker build -t="scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER" .
+docker tag scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER scriptnull/wordist.xyz:latest
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push scriptnull/wordist.xyz:build.travis.$TRAVIS_BUILD_NUMBER;
+docker push scriptnull/wordist.xyz:latest
